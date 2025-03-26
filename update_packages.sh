466608e6cf4d755beb83fac59b95c8e2883026f6
Rob Coenen <coenen.rob@gmail.com>
2025-03-25 22:58:14 -0400

    Removed temporary package renaming scripts

--- update_packages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find app/src/main -name "*.kt" -exec sed -i "" "s/package com.songshifter/package com.songshifter.app/g" {} \;
-find app/src/test -name "*.kt" -exec sed -i "" "s/package com.songshifter/package com.songshifter.app/g" {} \;
-find app/src/main -name "*.kt" -exec sed -i "" "s/import com.songshifter\./import com.songshifter.app./g" {} \;
-find app/src/test -name "*.kt" -exec sed -i "" "s/import com.songshifter\./import com.songshifter.app./g" {} \;
